6dae31fce9c4f6ea584b4564bb44e16a52e6e662
Deepak <deepakkumarjha@outlook.com>
2020-03-18 01:30:52 +0530

    initial commit

---
+++ build/precache-manifest.5b1c9c5fd013facfcfbd5e26677fff14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0fb74068faa1f2c66376346e87282c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e2733c3c9b027bb04d8",
+    "url": "/static/css/main.ffa29014.chunk.css"
+  },
+  {
+    "revision": "9c173f4a034ee1caee2c",
+    "url": "/static/js/2.555d9f10.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.555d9f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2733c3c9b027bb04d8",
+    "url": "/static/js/main.cfba95f5.chunk.js"
+  },
+  {
+    "revision": "e393215a7b4e55033a9f",
+    "url": "/static/js/runtime-main.18c7c3b7.js"
+  },
+  {
+    "revision": "abf8419c08cfd929ae4fe53a1916d21b",
+    "url": "/static/media/bizlogo.abf8419c.png"
+  }
+]);
---
+++ src/components/header/ISPInfo.js
@@ -0,0 +1,6 @@
+import React from 'react'
+function ISPInfo (){
+    return <div> <span> Total Isp: </span> <span> Api Hits: </span></div>
+}
+
+export default ISPInfo;
